a2b99facffd62663edd08858f73339b84123e1ec
Jose Hinojo <burningremedy@gmail.com>
2020-08-31 23:41:30 -0500

    Prepare Credential Selenium Tests

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/BrowserTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/BrowserTests.java
@@ -201,7 +201,10 @@ public class BrowserTests {
     }
 
     //TODO Write a Selenium test that logs in an existing user, creates a credential and verifies that the credential details are visible in the credential list.
+    @Test
+    public void LoginAndCreateCredentialThenVerify(){
 
+    }
 
     //TODO Write a Selenium test that logs in an existing user with existing credentials, clicks the edit credential button on an existing credential, changes the credential data, saves the changes, and verifies that the changes appear in the credential list.
 
--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/Pages/CredentialPage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/Pages/CredentialPage.java
@@ -1,4 +1,99 @@
 package com.udacity.jwdnd.course1.cloudstorage.Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class CredentialPage {
+
+    @FindBy(id = "add-credential")
+    WebElement addCredentialButton;
+
+    @FindBy(id = "credential-url")
+    WebElement inputUrl;
+
+    @FindBy(id = "credential-username")
+    WebElement inputUsername;
+
+    @FindBy(id = "credential-password")
+    WebElement inputPassword;
+
+    @FindBy(id = "credentialSubmit")
+    WebElement saveCredentialButton;
+
+    @FindBy(id = "credential-close")
+    WebElement cancelCredentialButton;
+
+    @FindBy(className = "edit-credential")
+    List<WebElement> editButtons;
+
+    @FindBy(className = "delete-credential")
+    List<WebElement> deleteButtons;
+
+    @FindBy(className = "credentialUrl")
+    List<WebElement> credentialUrls;
+
+    @FindBy(className = "credentialUsername")
+    List<WebElement> credentialUsername;
+
+    @FindBy(className = "credentialPassword")
+    List<WebElement> credentialPassword;
+
+    public CredentialPage(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+
+    public void clickAddButton(){
+        addCredentialButton.click();
+    }
+
+    public void setInputUrl(String url){
+        inputUrl.clear();
+        inputUrl.sendKeys(url);
+    }
+
+    public String getUrl(){
+        return inputUrl.getText();
+    }
+
+    public void setUsername(String username){
+        inputUsername.clear();
+        inputUsername.sendKeys(username);
+    }
+
+    public String getUsername(){
+        return inputUsername.getText();
+    }
+
+    public void setPassword(String password){
+        inputPassword.clear();
+        inputPassword.sendKeys(password);
+    }
+
+    public String getPassword(){
+        return inputPassword.getText();
+    }
+
+    public void clickButton(WebElement element){
+        element.click();
+    }
+
+    public void createCredential(String url, String username, String password){
+        clickAddButton();
+        setInputUrl(url);
+        setUsername(username);
+        setPassword(password);
+        saveCredentialButton.click();
+    }
+
+    public void editCredential(String url, String username, String password){
+        setInputUrl(url);
+        setUsername(username);
+        setPassword(password);
+        saveCredentialButton.click();
+    }
+
 }
